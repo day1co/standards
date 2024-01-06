ef7d4158e2c44e67c0cf38550e62ff9ad0f6ac8d
YoungTaek <soomtong@gmail.com>
2024-01-06 09:52:12 +0900

    prettier: trailingComma 옵션을 완화합니다.
    https://prettier.io/blog/2023/07/05/3.0.0.html#javascript

--- prettier-config/index.js
+++ prettier-config/index.js
@@ -4,4 +4,7 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
+  // prettier 3.x has breaking changes with trailing commas
+  // we prefer to keep the old behavior
+  trailingComma: 'es5'
 };
